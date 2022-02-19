c9b6643e968e5bb07d688522226f9f09b33efb93
maicaosgz <malconaugusto@outlook.com>
2022-02-19 13:53:29 -0300

    feat: jsx-1 (aula feita.) react-iniciantes

--- react-iniciantes/ambiente-curso/appreact/src/App.js
+++ react-iniciantes/ambiente-curso/appreact/src/App.js
@@ -1,10 +1,37 @@
-import React from "react"
-const App = ()=> {
+import React from "react";
+
+const titulo = <h1>Esse é um titulo</h1>;
+
+const App = () => {
+  const random = Math.random();
+  const ativo = true;
+
+  function mostrarNome(sobrenome) {
+    return " Malcon" + " " + sobrenome;
+  }
+
+  const carro = {
+    marca: "Ford",
+    rodas: 4,
+  };
+  const estiloP = {
+    color: "#cecece",
+    fontSize: "25px",
+  };
+
   return (
-    <div>
-      Meu App
-    </div>
+    <>
+      {titulo}
+      <p>
+        {false ? "testando true" : "testando false"} - 10
+        {mostrarNome("Augusto")}
+      </p>
+      <p style={estiloP}>{carro.marca}</p>
+      <p>{carro.rodas}</p>
+      <p style={estiloP}>{new Date().getFullYear()}</p>
+      <p className={ativo ? "ativo" : "inativo"}>{random * 400}</p>
+    </>
   );
-}
+};
 
 export default App;
