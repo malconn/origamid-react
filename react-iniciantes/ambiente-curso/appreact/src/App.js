110cbc4abb67ec7ac0dfcf0c81ca4e265f7c465c
Malcon Augusto Souza de Miranda <malcon.miranda@br.clara.net>
2022-04-26 11:53:18 -0300

    feat: useState (comecando exercicio) - react-iniciantes

--- react-iniciantes/ambiente-curso/appreact/src/App.js
+++ react-iniciantes/ambiente-curso/appreact/src/App.js
@@ -1,21 +1,36 @@
-import React,{useState} from "react";
+// Os links abaixo puxam dados de um produto em formato JSON
+// https://ranekapi.origamid.dev/json/api/produto/tablet
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// Crie uma interface com 3 botões, um para cada produto.
+// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
+// Mostre apenas um produto por vez
+// Mostre a mensagem carregando... enquanto o fetch é realizado
+import React, { useState } from 'react'
+
 const App = () => {
-  const [contar,setContar] = useState(1);
-  const [items,setItems] = useState(['Item 1']);
-
-  function handleClick(){
-    setContar((contar)=> {
-      return contar + 1
-    })
-    setItems((items)=>[...items, 'Item ' + (contar + 1)])
+  const [dados,setDados]= useState(null);
+  const [baseUrl,setBaseUrl] = useState('https://ranekapi.origamid.dev/json/api/produto/');
+  function handleClick(event){
+    setDados(event.target.innerText);
   }
-
+  function handleApi(baseUrl){
+    console.log(baseUrl)
+    fetch(baseUrl,{
+      method:'GET',
+    })
+    // .then(resp=> JSON.stringify(resp))
+    .then(resp =>console.log(resp))
+    // return resp.json();
+  }
   return (
     <div>
-      {items.map(item => <li key={item}>{item}</li>)}
-      <button onClick={handleClick}>{contar}</button>
+      {dados!==null ? handleApi(baseUrl+dados): ''}
+      <button onClick={handleClick}>tablet</button>
+      <button onClick={handleClick}>notebook</button>
+      <button onClick={handleClick}>smartphone</button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
