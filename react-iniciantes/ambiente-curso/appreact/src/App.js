593be32c37f3ea0f612ced2ccb6a468954131e9e
maicaosgz <malconaugusto@outlook.com>
2022-04-28 00:54:26 -0300

    feat: useState (continuidade do exercicio) - react-iniciantes

--- react-iniciantes/ambiente-curso/appreact/src/App.js
+++ react-iniciantes/ambiente-curso/appreact/src/App.js
@@ -6,31 +6,36 @@
 // Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
 // Mostre apenas um produto por vez
 // Mostre a mensagem carregando... enquanto o fetch é realizado
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const App = () => {
-  const [dados,setDados]= useState(null);
-  const [baseUrl,setBaseUrl] = useState('https://ranekapi.origamid.dev/json/api/produto/');
-  function handleClick(event){
+  const [dados, setDados] = useState("");
+  const baseUrl = "https://ranekapi.origamid.dev/json/api/produto/";
+  const [loading, setLoading] = useState(false);
+  function handleClick(event) {
     setDados(event.target.innerText);
+    handleApi();
   }
-  function handleApi(baseUrl){
-    console.log(baseUrl)
-    fetch(baseUrl,{
-      method:'GET',
+  function handleApi() {
+    setLoading(true);
+    fetch(baseUrl + dados, {
+      method: "GET",
     })
-    // .then(resp=> JSON.stringify(resp))
-    .then(resp =>console.log(resp))
-    // return resp.json();
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setLoading(false);
+        console.log(loading);
+      });
   }
   return (
     <div>
-      {dados!==null ? handleApi(baseUrl+dados): ''}
       <button onClick={handleClick}>tablet</button>
       <button onClick={handleClick}>notebook</button>
       <button onClick={handleClick}>smartphone</button>
+      {loading ? <p>Carregando...</p> : ""}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
