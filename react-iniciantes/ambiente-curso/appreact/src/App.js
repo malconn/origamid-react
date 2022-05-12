f357d4fbe0a151877cc311b46e98e115fec8e46f
maicaosgz <malconaugusto@outlook.com>
2022-05-11 22:49:11 -0300

    feat: useEffect (aula feita.) - react-iniciantes

--- react-iniciantes/ambiente-curso/appreact/src/App.js
+++ react-iniciantes/ambiente-curso/appreact/src/App.js
@@ -1,44 +1,12 @@
-// Os links abaixo puxam dados de um produto em formato JSON
-// https://ranekapi.origamid.dev/json/api/produto/tablet
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// Crie uma interface com 3 botões, um para cada produto.
-// Ao clicar no botão faça um fetch a api e mostre os dados do produto na tela.
-// Mostre apenas um produto por vez
-// Mostre a mensagem carregando... enquanto o fetch é realizado
-import React, { useState } from "react";
+import React from "react";
 import Produto from "./Produto";
 
 const App = () => {
-  const [loading, setLoading] = useState(false);
-  const [info, setInfo] = useState(null);
-  function handleClick(event) {
-    handleApi(event.target.innerText);
-  }
-  async function handleApi(dados) {
-    setLoading(true);
-    const response = await fetch(
-      `https://ranekapi.origamid.dev/json/api/produto/${dados}`,
-      {
-        method: "GET",
-      }
-    );
-    const json = await response.json();
-    setLoading(false);
-    setInfo(json);
-  }
+  const [ativo, setAtivo] = React.useState(false);
   return (
     <div>
-      <button style={{ margin: ".5rem" }} onClick={handleClick}>
-        tablet
-      </button>
-      <button style={{ margin: ".5rem" }} onClick={handleClick}>
-        notebook
-      </button>
-      <button style={{ margin: ".5rem" }} onClick={handleClick}>
-        smartphone
-      </button>
-      {loading ? <p>Carregando...</p> : <Produto info={info} />}
+      {ativo && <Produto />}
+      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
     </div>
   );
 };
--- react-iniciantes/ambiente-curso/appreact/src/Produto.js
+++ react-iniciantes/ambiente-curso/appreact/src/Produto.js
@@ -1,28 +1,18 @@
 import React from "react";
 
-const Produto = ({ info }) => {
+const Produto = () => {
+  React.useEffect(() => {
+    function handleScroll(event) {
+      console.log(event);
+    }
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div>
-      {info?.vendido == "true" && (
-        <span style={{ color: "#fff", background: "red" }}>
-          Este produto já foi vendido
-        </span>
-      )}
-      <h1>{info?.nome}</h1>
-      <img
-        key={info?.fotos[0]?.titulo}
-        src={info?.fotos[0]?.src}
-        alt={info?.fotos[0]?.titulo}
-      />
-      <h3>
-        {info?.preco &&
-          Number(info?.preco).toLocaleString("pt-BR", {
-            style: "currency",
-            currency: "BRL",
-          })}
-      </h3>
-      <h4>{info?.usuario_id}</h4>
-      <p>{info?.descricao}</p>
+    <div style={{ height: "200vh" }}>
+      <p>Meu produto</p>
     </div>
   );
 };
