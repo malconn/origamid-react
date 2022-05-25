3668e6f97b12075416572393cf7e7ebc2ba5c166
malconn <malconaugusto@outlook.com>
2022-05-24 23:11:04 -0300

    feat: useState (comecando exercicio de localstorage) -react-iniciantes

--- react-iniciantes/ambiente-curso/appreact/src/App.js
+++ react-iniciantes/ambiente-curso/appreact/src/App.js
@@ -1,12 +1,39 @@
+// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
+// Defina o produto clicado como uma preferência do usuário no localStorage
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
+
 import React from "react";
 import Produto from "./Produto";
 
 const App = () => {
-  const [ativo, setAtivo] = React.useState(false);
+  const [dado,setDado] = React.useState('');
+  const [showDado,setShowDado] = React.useState('');
+
+  function handleClick(event){
+    event.preventDefault()
+    const dadoToAPI = event.target.innerText;
+    setDado(dadoToAPI)
+  }
+
+  React.useEffect(()=>{
+    fetch(`https://ranekapi.origamid.dev/json/api/produto/${dado}`)
+    .then((result)=> result.json())
+    .then((result)=> setShowDado(result))
+  },[dado])
+
   return (
     <div>
-      {ativo && <Produto />}
-      <button onClick={() => setAtivo(!ativo)}>Ativar</button>
+      <h1>Preferência: {showDado?.nome}</h1>
+      <button style={{marginRight:'2rem',}} onClick={handleClick}>
+        smartphone
+      </button>
+      <button onClick={handleClick}>
+        notebook
+      </button>
+      <Produto nome={showDado?.nome} preco={showDado?.preco}/>
     </div>
   );
 };
--- react-iniciantes/ambiente-curso/appreact/src/Produto.js
+++ react-iniciantes/ambiente-curso/appreact/src/Produto.js
@@ -1,18 +1,11 @@
 import React from "react";
 
-const Produto = () => {
-  React.useEffect(() => {
-    function handleScroll(event) {
-      console.log(event);
-    }
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+const Produto = ({nome,preco}) => {
+  
   return (
-    <div style={{ height: "200vh" }}>
-      <p>Meu produto</p>
+    <div>
+      <h2>{nome}</h2>
+      <p>{preco}</p>
     </div>
   );
 };
