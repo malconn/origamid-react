cb52ffe99d918697ffca908320b5ab94707d6d70
malconn <malconaugusto@outlook.com>
2022-07-11 22:18:56 -0300

    feat: primeiro exeplo usando React.useRef (aula feita). react-iniciantes

--- react-iniciantes/ambiente-curso/appreact/src/App.js
+++ react-iniciantes/ambiente-curso/appreact/src/App.js
@@ -1,39 +1,22 @@
-// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
-// https://ranekapi.origamid.dev/json/api/produto/notebook
-// https://ranekapi.origamid.dev/json/api/produto/smartphone
-// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
-// Defina o produto clicado como uma preferência do usuário no localStorage
-// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
-
 import React from "react";
-import Produto from "./Produto";
 
 const App = () => {
-  const [dado,setDado] = React.useState(null);
-
-  function handleClick(event){
-    event.preventDefault()
-    const dadoToAPI = event.target.innerText;
-    setDado(dadoToAPI)
+  const [comentarios, setComentarios] = React.useState([]);
+  const [input,setInput] = React.useState('');
+  const inputElement = React.useRef();
+  function handleClick(){
+    setComentarios([...comentarios,input]);
+    setInput('');
+    inputElement.current.focus()
   }
-  React.useEffect(()=>{
-    const dadoToFecth = window.localStorage.getItem('preferencia')
-    dadoToFecth !== null && setDado(dadoToFecth)
-  },[])
-  React.useEffect(()=>{
-    dado !== null && window.localStorage.setItem('preferencia',dado)
-  },[dado])
-
   return (
     <div>
-      <h1>Preferência: {dado}</h1>
-      <button style={{marginRight:'2rem',}} onClick={handleClick}>
-        smartphone
-      </button>
-      <button onClick={handleClick}>
-        notebook
-      </button>
-      <Produto dado={dado}/>
+      <ul>
+        {comentarios.map((comentario) => <li key={(comentario)}>{comentario}</li>)}
+      </ul>
+      <input type='text' ref={inputElement} value={input} onChange={({target})=> setInput(target.value)}/>
+      <br />
+      <button onClick={handleClick}>Enviar</button>
     </div>
   );
 };
